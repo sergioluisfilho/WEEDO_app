df90ca953f7f29f01f8dae17b1efed0395a08071
sergioluisfilho <sergioluisfilho@hotmail.com>
2021-08-02 23:01:13 -0300

    cep data request

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  TextInput,
+} from "react-native";
+import axios from "axios";
+
 import { useCart } from "../context/CartContext";
 
 function Cart({ navigation }) {
+  const [CEP, setCEP] = React.useState(null);
+
   const { items, setItems } = useCart();
-  console.log(items);
+  //console.log(items);
+
+  function getAddressData() {
+    axios
+      .get(`https://viacep.com.br/ws/${CEP}/json`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   function checkout() {
     setItems([]);
@@ -22,9 +45,33 @@ function Cart({ navigation }) {
   return (
     <View>
       <Text>{items.length} itens</Text>
+      <View style={styles.cepContainer}>
+        <TextInput
+          placeholder="Seu CEP"
+          keyboardType="numeric"
+          style={styles.input}
+          onChangeText={setCEP}
+          value={CEP}
+        />
+        <Button title="Calcular" onPress={getAddressData} />
+      </View>
+
       <Button title="Finalizar Compra" onPress={checkout} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cepContainer: {
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default Cart;
