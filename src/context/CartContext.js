30af9ff8f2ef666d609e2628e60c3e15446625d3
sergioluisfilho <sergioluisfilho@hotmail.com>
2021-08-06 01:05:41 -0300

    async Store Cart

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const CartContext = createContext();
 
@@ -6,6 +7,24 @@ export default function CartProvider({ children }) {
   const [items, setItems] = useState([]);
   const [totalValue, setTotalValue] = useState(0);
   const [itemsQuantity, setItemsQuantity] = useState(0);
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@storage_Key");
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@storage_Key", jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
 
   function calculeAmount() {
     var total = items.reduce((total, item) => {
@@ -18,12 +37,20 @@ export default function CartProvider({ children }) {
     var total = items.reduce((total, item) => total + 1 * item.quantity, 0);
     setItemsQuantity(total);
   }
+  useEffect(() => {
+    getData().then((data) => {
+      if (data) {
+        setItems(data);
+      }
+    });
+  }, []);
 
   useEffect(() => {
-    //console.log(items);
     calculeAmount();
+    storeData(items);
     calculateItemsQuantity();
   }, [items]);
+
   return (
     <CartContext.Provider
       value={{
